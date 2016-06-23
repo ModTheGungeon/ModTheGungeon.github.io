e9fb1b1e316ed84d161d780b7a3dda5860c5a7d8
Maik Macho <nuda1998@gmail.com>
2016-06-23 20:05:37 +0200

    Add deploy_mod.sh

---
+++ deploy_mod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'This bash script will be used by the default Travis CI configuration to "deploy" (upload) your mod.'
+echo 'You should use your own script here! (This will be updated with a functional example soon(tm).)'
