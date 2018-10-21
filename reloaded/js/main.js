a4f5c69561e59905dab922cee37c9a40891edd34
Zatherz <zatherz@linux.pl>
2018-10-21 17:10:03 +0200

    Move the Reloaded WIP website here

---
+++ reloaded/js/main.js
@@ -0,0 +1,42 @@
+/*
+jsonp("//example.com/a?callback=", {
+    onSuccess: function(json) {
+    },
+    onTimeout: function(){
+    },
+    timeout: 5000
+});
+*/
+var jsonp = function(src, options) {
+    options = options || { };
+    options.callback = options.callback || "callback",
+    options.onSuccess = options.onSuccess || function() { },
+    options.onTimeout = options.onTimeout || function() { },
+    options.timeout = options.timeout || 1000;
+
+    var timeoutTrigger = window.setTimeout(function() {
+        options.onTimeout();
+        document.head.removeChild(script);
+    }, options.timeout);
+
+    window[options.callback] = function(data) {
+        window.clearTimeout(timeoutTrigger);
+        options.onSuccess(data);
+        document.head.removeChild(script);
+    };
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = src + options.callback;
+    document.head.appendChild(script);
+};
+
+var main = document.getElementById("main");
+var mainY;
+var updateMainY = function() {
+    mainY = main.offsetTop;
+};
+updateMainY();
+setTimeout(updateMainY, 200);
+window.addEventListener("resize", updateMainY);
