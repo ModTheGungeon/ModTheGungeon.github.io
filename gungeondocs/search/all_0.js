b25c9b54727eb79ea6e4ef36aede188df304f9fa
Zatherz <zatherz@linux.pl>
2019-08-27 15:47:29 +0200

    Intro Page #2

--- gungeondocs/search/all_0.js
+++ gungeondocs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['enter_20the_20gungeon_20auto_2dgenerated_20docs_0',['Enter the Gungeon Auto-Generated Docs',['../md_intro.html',1,'']]]
+  ['enter_20the_20gungeon_20auto_2dgenerated_20docs_0',['Enter the Gungeon Auto-Generated Docs',['../index.html',1,'']]]
 ];
