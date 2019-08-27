8978b145cfc5e5b06cdd07dc64bb1171a346d581
Zatherz <zatherz@linux.pl>
2019-08-27 15:46:39 +0200

    Intro page

---
+++ gungeondocs/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['enter_20the_20gungeon_20auto_2dgenerated_20docs_1',['Enter the Gungeon Auto-Generated Docs',['../md_intro.html',1,'']]]
+];
--- gungeondocs/search/searchdata.js
+++ gungeondocs/search/searchdata.js
@@ -1,39 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefghijklmnopqrstuvwxyz~",
-  1: "_abcdefghijklmnopqrstuvwyz",
-  2: "_abcdefghijklmnopqrstuvwyz",
-  3: "_abcdefghijklmnopqrstuvwxyz~",
-  4: "_abcdefghijklmnopqrstuvwxyz",
-  5: "abcdefghiklmnopqrstuvw",
-  6: "_abcdefghijklmnopqrstuvwxyz",
-  7: "abcdefghijklmnopqrstuvwxyz",
-  8: "abcdefghiklmoprstvz"
+  0: "e",
+  1: "e"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "enums",
-  6: "enumvalues",
-  7: "properties",
-  8: "events"
+  1: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Enumerations",
-  6: "Enumerator",
-  7: "Properties",
-  8: "Events"
+  1: "Pages"
 };
 
